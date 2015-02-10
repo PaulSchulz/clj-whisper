4370e2704d370569313de8b6978c7fcb743c15a8
Kirill Kondratenko <cybem@cybem.info>
2015-02-10 18:43:51 +0300

    Redesign fetcher

--- src/clj_whisper/core.clj
+++ src/clj_whisper/core.clj
@@ -27,6 +27,8 @@
   (set/map-invert aggregation-type-to-method))
 (def ^:const aggregation-methods (vals aggregation-type-to-method))
 
+(def ^:const epoch-future 2085436800)
+
 (defn- read-struct
   "Read structure."
   [^RandomAccessFile file format size keys]
@@ -73,23 +75,11 @@
 (defn read-info
   "Read info."
   [^String file]
-  (let [ra-file (RandomAccessFile. file "r")
-        result (read-info-ra ra-file file)]
-    (.close ra-file)
-    result))
-
-(defn- calc-interval
-  "Calculate interval."
-  [time seconds-per-point]
-  (int (+ (- time (mod time seconds-per-point)) seconds-per-point)))
-
-(defn- calc-offset
-  "Calculate offset."
-  [interval base-interval seconds-per-point offset size]
-  (let [time-distance (- interval base-interval)
-        point-distance (/ time-distance seconds-per-point)
-        byte-distance (* point-distance point-size)]
-    (+ offset (mod byte-distance size))))
+  (let [ra-file (RandomAccessFile. file "r")]
+    (try
+      (read-info-ra ra-file file)
+      (finally
+        (.close ra-file)))))
 
 (defn- read-base-interval
   "Read base interval."
@@ -99,21 +89,18 @@
 
 (defn- read-series
   "Read series."
-  [^RandomAccessFile file ^String filename from-offset until-offset offset size]
-  (.seek file from-offset)
-  (if (< from-offset until-offset)
-    (let [buf-size (- until-offset from-offset)
-          bytes (byte-array buf-size)]
-      (.readFully file bytes 0 buf-size)
-      (vec bytes))
-    (let [archive-end (+ offset size)
-          part1-size (- archive-end from-offset)
-          part2-size (- until-offset offset)
-          bytes (byte-array (+ part1-size part2-size))]
-      (.readFully file bytes 0 part1-size)
-      (.seek file offset)
-      (.readFully file bytes part1-size part2-size)
-      (vec bytes))))
+  [^RandomAccessFile file ^String filename offset size]
+  (.seek file offset)
+  (let [bytes (byte-array size)]
+    (.readFully file bytes 0 size)
+    (vec bytes)))
+
+(defn align-time
+  "Align time to seconds per point."
+  [time seconds-per-point]
+  (if (not= time nil)
+    (-> time (quot seconds-per-point) (* seconds-per-point))
+    nil))
 
 (defn- get-point
   "Get point."
@@ -123,26 +110,34 @@
         ppoint (subvec raw-series start end)]
     (struct/unpack point-format ppoint)))
 
+(defn- limit-series
+  "Limit time series."
+  [series from-time until-time]
+  (if (and (= from-time nil)
+           (= until-time nil))
+    series
+    (let [from-time (if from-time from-time 0)
+          until-time (if until-time until-time epoch-future)]
+      (filter #(let [time (first %)] (and (>= time from-time)
+                                          (<= time until-time)))
+              series))))
+
 (defn fetch-archive-seq-ra
   "Fetch archive from a random access file."
   [^RandomAccessFile file ^String filename archive from-time until-time]
   (let [{:keys [offset seconds-per-point size]} archive
-        from-interval (calc-interval from-time seconds-per-point)
-        until-interval (calc-interval until-time seconds-per-point)
+        from-interval (align-time from-time seconds-per-point)
+        until-interval (align-time until-time seconds-per-point)
         {:keys [base-interval]} (read-base-interval file filename offset)]
     (if (= base-interval 0)
       {:series []
        :from from-interval
        :until until-interval
        :step seconds-per-point}
-      (let [from-offset (calc-offset from-interval base-interval
-                                     seconds-per-point offset size)
-            until-offset (calc-offset until-interval base-interval
-                                      seconds-per-point offset size)
-            raw-series (read-series file filename from-offset until-offset offset
-                                    size)
+      (let [raw-series (read-series file filename offset size)
             points (/ (count raw-series) point-size)
-            series (map (partial get-point raw-series) (range points))]
+            full-series (map (partial get-point raw-series) (range points))
+            series (limit-series full-series from-interval until-interval)]
         {:series series
          :from from-interval
          :until until-interval
@@ -151,10 +146,11 @@
 (defn fetch-archive-seq
   "Fetch archive."
   [^String file archive from-time until-time]
-  (let [ra-file (RandomAccessFile. file "r")
-        result (fetch-archive-seq-ra ra-file file archive from-time until-time)]
-    (.close ra-file)
-    result))
+  (let [ra-file (RandomAccessFile. file "r")]
+    (try
+      (fetch-archive-seq-ra ra-file file archive from-time until-time)
+      (finally
+        (.close ra-file)))))
 
 (defn get-paths
   "Get paths."
